c56e61a4896dede95f00eff928c9096bbac9b264
Jejugo <jeffgoes22@gmail.com>
2018-10-26 00:56:56 -0300

    added modal configurations

--- src/components/FilterHeader.js
+++ src/components/FilterHeader.js
@@ -7,11 +7,30 @@ class FilterHeader extends Component {
       <div>
         <div className="nav-content filterHeader z-depth-2">
           <div className="container">
-            <a className="waves-effect waves-light btn-small btn btn-filter">Datas</a>
-            <a className="waves-effect waves-light btn-small btn btn-filter">Hóspedes</a>
-            <a className="waves-effect waves-light btn-small btn btn-filter">Comodidades</a>
-            <a className="waves-effect waves-light btn-small btn btn-filter">Preço</a>
-            <a className="waves-effect waves-light btn-small btn btn-filter">Ratings</a>
+            <a href="#datas" className="waves-effect waves-light btn-small btn btn-filter modal-trigger">Datas</a>
+            <a href="#hospedes" className="waves-effect waves-light btn-small btn btn-filter modal-trigger">Hóspedes</a>
+            <a href="#comodidades" className="waves-effect waves-light btn-small btn btn-filter modal-trigger">Comodidades</a>
+            <a href="#preco" className="waves-effect waves-light btn-small btn btn-filter modal-trigger">Preço</a>
+            <a href="#ratings" className="waves-effect waves-light btn-small btn btn-filter modal-trigger">Ratings</a>
+          </div>
+        </div>
+        <div className="modal" id="datas">
+          teste
+        </div>
+        <div className="modal" id="hospedes">
+          <div className="modal-content">
+            <div className="col s12 hospedes-line">
+              <span><h5 style={{ display: 'inline' }}>Adultos</h5></span>
+              <a className="btn-floating btn-small waves-effect waves-light right filter-modal"><i className="material-icons">remove_circle_outline</i></a>
+              <h5 className="right">5</h5>
+              <a className="btn-floating btn-small waves-effect waves-light right filter-modal"><i className="material-icons">add_circle_outline</i></a>
+            </div>
+            <div className="col s12 hospedes-line">
+              <span><h5 style={{ display: 'inline' }}>Crianças</h5></span>
+              <a className="btn-floating btn-small waves-effect waves-light right filter-modal"><i className="material-icons">remove_circle_outline</i></a>
+              <h5 className="right">5</h5>
+              <a className="btn-floating btn-small waves-effect waves-light right filter-modal"><i className="material-icons">add_circle_outline</i></a>
+            </div>
           </div>
         </div>
       </div>
